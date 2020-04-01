0fa53b77a2bb66a1d4714e138a1471e858fc7824
novawei <913252732@qq.com>
2020-04-01 09:28:06 +0800

    category

--- src/store/actions/actionTypes.ts
+++ src/store/actions/actionTypes.ts
@@ -1,2 +1,6 @@
+
+// module1
 export const ACT_1 = 'ACT_ 1'
+
+// module2
 export const ACT_2 = 'ACT_ 2'
--- src/store/actions/index.ts
+++ src/store/actions/index.ts
@@ -1,10 +1,12 @@
 import * as actionTypes from './actionTypes'
 
+// module1
 export const doAct1 = (payload: string) => ({
   type: actionTypes.ACT_1,
   payload
 })
 
+// module2
 export const doAct2 = (payload: number) => ({
   type: actionTypes.ACT_2,
   payload
